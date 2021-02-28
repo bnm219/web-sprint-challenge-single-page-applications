3ff47bd7310c9eeb840da27538203f8238298474
Bri <brittanymorris@MacBook-Pro.local>
2021-02-28 16:33:04 -0700

    Finished adding the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 import {
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import PizzaForm from './PizzaForm'
+import PizzaForm from './PizzaForm';
+import * as yup from 'yup';
+import axios from 'axios'
+import formSchema from './formSchema'
+
+
+//initial State
+const initalFormValues = {
+  //text input
+  name: '',
+  email: '',
+  password: '',
+  //checkbox
+  termsOfService: false,
+}
+const initialFormErrors = {
+  name:'',
+  email:'',
+  password: ''
+}
+
+const initialUsers = []
+const initalDisabled = false
 
 const App = () => {
+   //states
+   const [users, setUsers] = useState(initialUsers);
+   const [formValues, setFormValues] = useState(initalFormValues);
+   const [formErrors, setFormErrors] = useState(initialFormErrors);
+   const [disabled, setDisabled] = useState(initalDisabled);
+ 
+   //helper functions
+   const getUsers = () =>{
+     axios.get(`https://reqres.in/api/users`)
+     .then(res => setUsers(res.data.data))
+     .catch(err => console.log('GET Error: ', err))
+   }
+ 
+   const postNewUser = newUser =>{
+     axios.post(`https://reqres.in/api/users`, newUser)
+     .then(res =>{
+       setUsers([res.data, ...users])
+     })
+     .catch(err =>{
+       console.log('POST Error: ', err)
+     })
+   }
+ 
+ 
+   //event handlers
+   const inputChange = (name, value) =>{
+     yup.reach(formSchema, name)
+       .validate(value)
+       .then(() =>{
+         //happy path
+         setFormErrors({...formErrors, [name]:''})
+       })
+       .catch(err =>{
+         //sad path
+         setFormErrors({...formErrors, [name]: err.errors[0]})
+       })
+     setFormValues({...formValues, [name]: value})
+   }
+ 
+   const formSubmit = () =>{
+     const newUser = {
+       //text input
+       name: formValues.name.trim(),
+       email: formValues.email.trim(),
+       password: formValues.password.trim(),
+       //checkbox
+       termsOfService: formValues.termsOfService //bool
+     }
+     postNewUser(newUser);
+   }
+ 
+   //side effects
+   useEffect(() =>{
+     getUsers()
+   }, []);
+ 
+   useEffect(() => {
+     formSchema.isValid(formValues)
+     .then(valid => setDisabled(!valid))
+   }, [formValues]);
   return (
     <div>
       <h1>Lambda Eats</h1>
@@ -19,7 +101,13 @@ const App = () => {
 
       <Switch>
         <Route path = '/pizza-form'>
-          <PizzaForm />
+          <PizzaForm 
+          values = {formValues}
+          change = {inputChange}
+          submit = {formSubmit}
+          disabled = {disabled}
+          errors = {formErrors}
+          />
         </Route>
         <Route exact path = '/'/>
       </Switch>
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,9 +1,107 @@
 import React from 'react'
 
+export default function PizzaForm(props){
+    const {
+        values,
+        submit,
+        change,
+        disabled,
+        errors,
+    } = props
 
-export default function PizzaForm(){
+    const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+  }
 
-    return (
-        <div>Pizza Form</div>
+  const onChange = evt => {
+    const { name, value, type, checked } = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value
+    change(name, valueToUse)
+  }
+
+
+    return(
+        <form className='form container' onSubmit={onSubmit}>
+            <div className='form-group submit'>
+                <h2>Order Pizza</h2>
+
+                {/* 🔥 DISABLE THE BUTTON */}
+                <button id ='submitBtn' disabled={disabled}>submit</button>
+
+                <div className='errors'>
+                {/* 🔥 RENDER THE VALIDATION ERRORS HERE */}
+                <div>{errors.username}</div>
+                <div>{errors.email}</div>
+                <div>{errors.role}</div>
+                <div>{errors.civil}</div>
+                </div>
+            </div> 
+            <div className = 'form-group inputs'>
+                <h4>User information</h4>
+                {/* ////////// TEXT INPUTS ////////// */}
+                <label>Name
+                    <input 
+                        value = {values.name}
+                        onChange = {onChange}
+                        name = 'name'
+                        type = 'text'
+                    />
+                </label>
+                {/*/////////DROPDOWN//////*/}
+                <select 
+                onChange = {onChange}
+                id = 'roleSelect' 
+                name = 'role'
+                value = {values.size}>
+                <option value=''>Select</option>
+                <option value='10 inch'>10 "</option>
+                <option value='12 inch'>12 "</option>
+                <option value='16 inch'>16 "</option>
+                </select>
+                {/* ////////// CHECKBOXES ////////// */}
+                <label>Pepperonie
+                <input 
+                    type = 'checkbox'
+                    name = 'termsOfService'
+                    onChange = {onChange}
+                    checked = {values.pepperonie}
+                />
+                </label>
+                <label>Cheese
+                <input 
+                    type = 'checkbox'
+                    name = 'termsOfService'
+                    onChange = {onChange}
+                    checked = {values.cheese}
+                />
+                </label>
+                <label>Sausage
+                <input 
+                    type = 'checkbox'
+                    name = 'termsOfService'
+                    onChange = {onChange}
+                    checked = {values.sausage}
+                />
+                </label>
+                <label>Pineapple
+                <input 
+                    type = 'checkbox'
+                    name = 'termsOfService'
+                    onChange = {onChange}
+                    checked = {values.pineapple}
+                />
+                </label>
+                <label>Special Instructions
+                <input
+                    value={values.instructions}
+                    onChange={onChange}
+                    name='instructions'
+                    type='text'
+                />
+                </label>
+            </div>
+        </form>
     )
+
 }
